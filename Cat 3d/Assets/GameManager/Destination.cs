e0306d7d8071fca215c77161153a762e0d46a01a
James <kingdon909@gmail.com>
2020-07-11 22:54:44 -0400

    game manager

---
+++ Cat 3d/Assets/GameManager/Destination.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Destination : MonoBehaviour
+{
+    public UnityEvent OnLevelComplete;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("cat"))
+        {
+            OnLevelComplete.Invoke();
+        }
+    }
+}
---
+++ Cat 3d/Assets/GameManager/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private static int sceneNum = 1;
+    private GameObject cat;
+    private GameObject destination;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void ProceedToNextLevel()
+    {
+        SceneManager.LoadScene("level" + sceneNum++);
+        cat = GameObject.FindGameObjectWithTag("cat");
+        GameObject catStart = GameObject.FindGameObjectWithTag("catStart");
+        cat.transform.position = catStart.transform.position;
+        destination = GameObject.FindGameObjectWithTag("destination");
+    }
+}
