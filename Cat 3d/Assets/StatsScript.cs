5c3ed39eb71366fa9a8a3c5175e54f83b50f61f7
vuongle6@gmail.com <vle7@uncc.edu>
2020-07-11 21:05:40 -0400

    added Stats Screen scripting

---
+++ Cat 3d/Assets/StatsScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI.Extensions;
+
+public class StatsScript : MonoBehaviour
+{
+
+    UIPolygon statsIndicator;
+
+    [SerializeField] float str = 1f;
+    [SerializeField] float agi = 2f;
+    [SerializeField] float foc = 3f;
+    [SerializeField] float perc = 4f;
+    [SerializeField] float anger = 5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statsIndicator = GetComponent<UIPolygon>();
+        statsIndicator.VerticesDistances = new float[6] { perc * .2f, foc * .2f, agi * .2f, str * .2f, anger * .2f, perc * .2f };
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        statsIndicator.VerticesDistances = new float[6] { perc * .2f, foc * .2f, agi * .2f, str * .2f, anger * .2f, perc * .2f };
+    }
+}
