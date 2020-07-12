e70a6157664106e6e2586932bf3c0dc6d9269d0f
vuongle6@gmail.com <vle7@uncc.edu>
2020-07-12 11:57:07 -0400

    completed pouncing action

--- Cat 3d/Assets/Scripts/CatAIScripts/CatScript.cs
+++ Cat 3d/Assets/Scripts/CatAIScripts/CatScript.cs
@@ -8,14 +8,21 @@ public class CatScript : MonoBehaviour
 
     //stats go here
     [SerializeField] private float str = 10;
+    [SerializeField] private float spd = 10;
     [SerializeField] private float end = 10;
     [SerializeField] private float foc = 10;
     [SerializeField] private float anger = 10;
     [SerializeField] private float perc = 10;
-    [SerializeField] private float spd = 10;
-
+    public bool Grounded { get; set; }
     public float Exhaustion { get; set; }
+
+    public float Str { get => str; set => str = value; }
     public float Spd { get => spd; set => spd = value; }
+    public float End { get => end; set => end = value; }
+    public float Foc { get => foc; set => foc = value; }
+    public float Anger { get => anger; set => anger = value; }
+    public float Perc { get => perc; set => perc = value; }
+
     public float Excitement { get; set; }
     public float Irritability { get; set; }
     public Rigidbody Rb { get; set; }
@@ -34,6 +41,8 @@ public class CatScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
         //update mood
         //check for state changes based on mood
     }
