5b18c9df752632bd341d29c31c58b222cad5a9b4
jewingo <jewingo@ncsu.edu>
2020-07-12 14:34:23 -0400

    Added respawner script

--- Cat 3d/Assets/Respawner.cs
+++ Cat 3d/Assets/Respawner.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class Respawner : MonoBehaviour
 {
-    Vector3 checkpoint;
+    private Vector3 checkpoint;
+    [SerializeField]
+    private int steps;
+    private Rigidbody rb;
+    [SerializeField]
     // Start is called before the first frame update
     void Start()
     {
         checkpoint = Vector3.zero + transform.position;
+        rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.R))
+        {
+            respawn();
+        }
     }
 
     private void FixedUpdate()
     {
-        //if((transform.position - checkpoint))
+        
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 20f))
+            {
+                if(hit.distance < 2f && (transform.position - checkpoint).magnitude > steps)
+                    checkpoint = Vector3.zero + transform.position;
+            } else {
+                if (rb.velocity.y < -15f)
+                    respawn();
+            }
+    }
+
+    public void respawn()
+    {
+        transform.position = checkpoint;
+        rb.velocity = Vector3.zero;
     }
 }
