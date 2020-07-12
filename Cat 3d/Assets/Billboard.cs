647de7af00fb40e853a2eeed2089d314072c05f0
jewingo <jewingo@ncsu.edu>
2020-07-12 11:41:10 -0400

    Update costume switcher
    Updated to change expression and costume from CostumeSwitcher using updateExpression(string) and updateCostume(string)

--- Cat 3d/Assets/Billboard.cs
+++ Cat 3d/Assets/Billboard.cs
@@ -2,16 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
 public class Billboard : MonoBehaviour
 {
     // Start is called before the first frame update
     public Camera camera;
     public Transform camTransform;
-
+    Dictionary<string, GameObject> expressions;
+    string[] expressionKeys = {"alert", "angry", "asleep", "lazy", "normal"};
+    private string currentExpression = "normal";
     void Start()
     {
         if(camera != null)
             camTransform = camera.transform;
+        expressions = new Dictionary<string, GameObject>();
+    }
+
+    public void updateExpression(string expression)
+    {
+        if(expressions[expression] != null)
+        {
+            expressions[currentExpression].SetActive(false);
+            expressions[expression].SetActive(true);
+            currentExpression = expression;
+        } else
+        {
+            Debug.Log("Can't update expression; expression doesn't exist (alert, angry, asleep, lazy, normal)");
+        }
+        
     }
 
     // Update is called once per frame
---
+++ Cat 3d/Assets/CostumeTester.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CostumeTester : MonoBehaviour
+{
+    public costumeSwitcher cat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if(cat != null)
+        {
+            if(Input.GetKeyDown(KeyCode.UpArrow) {
+
+            }
+            if (Input.GetKeyDown(KeyCode.DownArrow) {
+
+            }
+        }
+        */
+        
+    }
+}
